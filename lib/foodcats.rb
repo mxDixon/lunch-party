68f9d2bb7771c21f076d813e5b673e4a25e85aac
mxDixon <christopher.dixon@careerbuilder.com>
2014-09-18 11:36:24 -0400

    food categories

---
+++ lib/foodcats.rb
@@ -0,0 +1,5 @@
+class FoodCats
+  def categories
+    ['American',' Asian',' Bakery','Barbecue','Breakfast & Brunch','Burgers','Cajun & Creole','Caribbean','Chinese','Cuban','Donuts','Fast Food','Hot Dogs & Sausages','Indian','Italian','Japanese','Korean','Latin American','Mexican','Noodle Shop','Pizza','Pub Food','Salad','Sandwiches & Subs','Seafood','Soup','Southern & Soul','Southwestern','Spanish','Steakhouse','Sushi','Tacos','Tex-Mex','Thai','Vegetarian','Vietnamese']
+  end
+end
