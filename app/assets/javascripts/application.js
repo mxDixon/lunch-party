1ba5224fc7cdbce7d0cc85f56f232c648f2d0c02
mxDixon <christopher.dixon@careerbuilder.com>
2014-12-12 01:42:14 -0500

    moving js cuz i'm lazy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,55 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
-//= require ajax_flash.js
 //= require_tree .
+
+// FLASH NOTICE ANIMATION
+var fade_flash = function() {
+  $("flash_notice").delay(5000).fadeOut("slow");
+  $("flash_success").delay(5000).fadeOut("slow");
+  $("flash_error").delay(5000).fadeOut("slow");
+  $("flash_alert").delay(5000).fadeOut("slow");
+  $("flash_warning").delay(5000).fadeOut("slow");
+};
+fade_flash();
+
+var show_ajax_message = function(msg, type) {
+  if(msg){
+    $("#flash-message").html('<div id="flash_'+type+'" class="'+flash_class_from_type(type)+'">'+msg+'</div>');
+    fade_flash();
+  }
+};
+
+$(document).ajaxComplete(function(event, request) {
+  var msg = request.getResponseHeader('X-Message');
+  var type = request.getResponseHeader('X-Message-Type');
+  show_ajax_message(msg, type); //use whatever popup, notification or whatever plugin you want
+});
+
+
+var flash_class_from_type = function(type){
+
+  cssClass = 'alert';
+
+  switch(type){
+    case 'notice':
+      cssClass = "alert alert-info";
+      break;
+    case 'success':
+      cssClass = "alert alert-success";
+      break;
+    case 'error':
+      cssClass = "alert alert-error";
+      break;
+    case 'alert':
+      cssClass = "alert alert-danger";
+      break;
+    case 'warning':
+      cssClass = "alert alert-warning";
+      break;
+    default:
+      cssClass = 'alert';
+  }
+
+  return cssClass
+};
