9c920ae4a34e57d78e27ee909f7d04f642ed23ed
mxDixon <christopher.dixon@careerbuilder.com>
2014-12-11 23:42:17 -0500

    uncommenting for pro

--- app/models/hipchat_messenger.rb
+++ app/models/hipchat_messenger.rb
@@ -3,33 +3,33 @@ require 'httparty'
 class HipchatMessenger
 
   def self.party_ready(party)
-    # party.people.each do |peep|
-    #   response = HTTParty.post("https://consumerprofile.hipchat.com/v2/user/#{peep.name}/message?auth_token=MuE1Ln34HkOgQ9SFIqynMbgBMrjGg3PVnrtLJrCb",
-    #                            :body => self.ready_message(party, peep),
-    #                            :headers => {'content-type' => 'text/plain'}
-    #   )
-    #   puts "#{peep} missed a message " + response.request +  " " + response.to_yaml if response.code != 204
-    # end
+    party.people.each do |peep|
+      response = HTTParty.post("https://consumerprofile.hipchat.com/v2/user/#{peep.name}/message?auth_token=MuE1Ln34HkOgQ9SFIqynMbgBMrjGg3PVnrtLJrCb",
+                               :body => self.ready_message(party, peep),
+                               :headers => {'content-type' => 'text/plain'}
+      )
+      puts "#{peep} missed a message " + response.request +  " " + response.to_yaml if response.code != 204
+    end
   end
 
   def self.leave_now(party)
-    # party.zombies.each do |z|
-    #   response = HTTParty.post("https://consumerprofile.hipchat.com/v2/user/#{z}/message?auth_token=MuE1Ln34HkOgQ9SFIqynMbgBMrjGg3PVnrtLJrCb",
-    #                            :body => self.leave_message,
-    #                            :headers => {'content-type' => 'text/plain'}
-    #   )
-    #   puts "#{z} missed a message " + response.request.to_yaml +  " " + response.to_yaml if response.code != 204
-    # end
+    party.zombies.each do |z|
+      response = HTTParty.post("https://consumerprofile.hipchat.com/v2/user/#{z}/message?auth_token=MuE1Ln34HkOgQ9SFIqynMbgBMrjGg3PVnrtLJrCb",
+                               :body => self.leave_message,
+                               :headers => {'content-type' => 'text/plain'}
+      )
+      puts "#{z} missed a message " + response.request.to_yaml +  " " + response.to_yaml if response.code != 204
+    end
   end
 
   def self.requeue(party)
-    # party.people.each do |p|
-    #   response = HTTParty.post("https://consumerprofile.hipchat.com/v2/user/#{p.name}/message?auth_token=MuE1Ln34HkOgQ9SFIqynMbgBMrjGg3PVnrtLJrCb",
-    #                            :body => self.requeue_message,
-    #                            :headers => {'content-type' => 'text/plain'}
-    #   )
-    #   puts "#{p} missed a message " + response.request.to_yaml +  " " + response.to_yaml if response.code != 204
-    # end
+    party.people.each do |p|
+      response = HTTParty.post("https://consumerprofile.hipchat.com/v2/user/#{p.name}/message?auth_token=MuE1Ln34HkOgQ9SFIqynMbgBMrjGg3PVnrtLJrCb",
+                               :body => self.requeue_message,
+                               :headers => {'content-type' => 'text/plain'}
+      )
+      puts "#{p} missed a message " + response.request.to_yaml +  " " + response.to_yaml if response.code != 204
+    end
   end
   private
 
